3cbae348a36da2f0c02de2e598647b9e0e0c7c30
Skynet_u <58257139+Skynet16@users.noreply.github.com>
2021-12-20 23:17:18 +0300

    fib_loop.py

--- fib_loop.py
+++ fib_loop.py
@@ -1,9 +1,20 @@
-fib1 = fib2 = 1
- 
-n = int(input())
- 
-print(fib1, fib2, end=' ')
- 
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end=' ')
+import time
+
+def fib(n):
+    if n <=2:
+        return 1
+    else:
+        prev = 0
+        next = 1
+        for i in range (0,n):
+            temp = next
+            next = prev + next
+            prev = temp
+            i = i + 1
+        return prev
+
+starttime = time.time()
+result = fib(int(input()))
+endtime = time.time() - starttime
+
+print(result,endtime)
